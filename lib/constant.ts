355173523f1e78823b288d7780e2ff2850b52507
Yeow Zi Qin <ziqinyeow@gmail.com>
2022-02-23 12:50:51 +0800

    update database schema

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,3 +1,5 @@
 export const TOKEN_SECRET = process.env.TOKEN_SECRET;
 export const USER = process.env.USER;
 export const PWD = process.env.PWD;
+export const EMAIL = process.env.EMAIL;
+export const PASSWORD = process.env.PASSWORD;
--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -2,6 +2,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { generateToken } from "lib/auth";
 import { prisma } from "lib/prisma";
 import bcrypt from "bcryptjs";
+import nodemailer from "nodemailer";
+import { nanoid } from "nanoid";
+import { EMAIL, PASSWORD } from "lib/constant";
 
 export default async function handler(
   req: NextApiRequest,
@@ -21,6 +24,7 @@ export default async function handler(
     phone_no,
     address,
     postcode,
+    state,
   } = req.body;
 
   switch (type) {
@@ -40,6 +44,7 @@ export default async function handler(
             phone_no,
             address,
             postcode,
+            state,
           },
         });
 
@@ -70,6 +75,89 @@ export default async function handler(
           .json({ verified: false, message: "User login unsuccessfully" });
       } catch (e) {
         return res.status(404).json({ message: "User has not sign up yet" });
+      }
+
+    case "req_tac":
+      const id = nanoid();
+      try {
+        const user = await prisma.user.update({
+          where: {
+            email,
+          },
+          data: {
+            fp_token: id,
+          },
+        });
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+        var transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: EMAIL,
+            pass: PASSWORD,
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+        });
+
+        var mailOptions = {
+          from: EMAIL,
+          to: email,
+          subject: "YOUR TAC NO.",
+          text: id,
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+            return res.status(400).json({ message: "Email not sent" });
+          } else {
+            console.log("Email sent: " + info.response);
+            return res.status(200).json({ message: "Check your email" });
+          }
+        });
+      } catch (error) {
+        console.log(error);
+
+        return res.status(400).json({ message: "Email not sent" });
+      }
+      return res.status(400).json({ message: "Email not sent" });
+
+    case "res_tac":
+      try {
+        const { code } = req.body;
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+        if (!user.fp_token) {
+          return res
+            .status(404)
+            .json({ message: "Please request the TAC again" });
+        }
+        if (user.fp_token === code) {
+          const salt = await bcrypt.genSalt(10);
+          const hashedPassword = await bcrypt.hash(password, salt);
+          await prisma.user.update({
+            where: {
+              email,
+            },
+            data: {
+              password: hashedPassword,
+            },
+          });
+          return res.status(200).json({ message: "Password updated" });
+        }
+      } catch (error) {
+        return res
+          .status(404)
+          .json({ message: "Error carrying out this task" });
       }
 
     default:
