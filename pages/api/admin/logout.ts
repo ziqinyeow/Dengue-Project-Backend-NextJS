753ac56591fcf8002e50233cc00c800da151e5db
Yeow Zi Qin <ziqinyeow@gmail.com>
2022-03-08 22:34:48 +0800

    test

--- pages/api/admin/logout.ts
+++ pages/api/admin/logout.ts
@@ -20,7 +20,7 @@ export default async function handler(
       path: "/",
     });
 
-    res.setHeader("Set-Cookie", serialised);
+    res.setHeader("Set-Cookie", serialised).redirect("/login");
 
     return res.status(200).json({ message: "ok" });
   }
