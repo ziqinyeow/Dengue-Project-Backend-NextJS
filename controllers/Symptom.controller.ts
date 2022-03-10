a408d388afeb6077b7d85047f6092b0d64dd4ee3
Yeow Zi Qin <ziqinyeow@gmail.com>
2022-03-10 11:40:45 +0800

    update symptom risk calculation error

--- controllers/Symptom.controller.ts
+++ controllers/Symptom.controller.ts
@@ -50,17 +50,17 @@ export const create = async (
     // 1 1 2 1 3 1 4 2 5 2 6 2 7 2 8 2 9 2 10 2 11 1
     // let risk_type = "";
     let indexes = 0;
-    indexes += 1; // fever
-    indexes += arr[3] <= 3 && arr[3] >= 1 ? 1 : 2; // vomitting
-    indexes += arr[5] <= 3 && arr[5] >= 1 ? 1 : 2; // diarrhoea
-    indexes += arr[7] === 1 ? 2 : 1; // stomach pain
-    indexes += arr[9] === 1 ? 2 : 1; // bleeding
-    indexes += arr[11] === 1 ? 2 : 1; // difficulty breathing
-    indexes += arr[13] === 1 ? 2 : 1; // fainting
-    indexes += arr[15] === 1 ? 2 : 1; // tired
-    indexes += arr[17] === 1 ? 2 : 1; // drowsy
-    indexes += arr[19] === 1 ? 2 : 1; // reduced urine
-    indexes += arr[21] === 1 ? 1 : 2; // reduced drinking
+    indexes += 1; // fever 1
+    indexes += arr[3] <= 3 && arr[3] >= 1 ? 1 : 2; // vomitting 2
+    indexes += arr[5] <= 3 && arr[5] >= 1 ? 1 : 2; // diarrhoea 3
+    indexes += arr[7] === 1 ? 2 : 1; // stomach pain 4
+    indexes += arr[9] === 1 ? 2 : 1; // bleeding 5
+    indexes += arr[11] === 1 ? 2 : 1; // difficulty breathing 6
+    indexes += arr[13] === 1 ? 2 : 1; // fainting 7
+    indexes += arr[15] === 1 ? 2 : 1; // tired 8
+    indexes += arr[17] === 1 ? 2 : 1; // drowsy 9
+    indexes += arr[19] === 1 ? 2 : 1; // reduced urine 10
+    indexes += arr[21] === 2 ? 2 : 1; // reduced drinking 11
 
     const risk_value = indexes;
     const risk_status = risk_value > 11 ? "dangerous" : "normal";
