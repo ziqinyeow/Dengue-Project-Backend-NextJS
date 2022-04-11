937eba2aa598ff22c01fd4a3147fc52d5a77389b
Yeow Zi Qin <ziqinyeow@gmail.com>
2022-04-11 16:23:40 +0800

    update

--- pages/api/user/patient.ts
+++ pages/api/user/patient.ts
@@ -1,7 +1,40 @@
 import { verifyAPI } from "lib/auth";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "lib/prisma";
-import dayjs from "dayjs";
+
+function time_diff(dateFuture: Date) {
+  const now = new Date();
+  // @ts-ignore
+  let diffInMilliSeconds = Math.abs(dateFuture - now) / 1000;
+
+  // calculate days
+  const days = Math.floor(diffInMilliSeconds / 86400);
+  diffInMilliSeconds -= days * 86400;
+  // console.log("calculated days", days);
+
+  // calculate hours
+  const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
+  diffInMilliSeconds -= hours * 3600;
+  // console.log("calculated hours", hours);
+
+  // calculate minutes
+  const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
+  diffInMilliSeconds -= minutes * 60;
+  // console.log("minutes", minutes);
+
+  let difference = "";
+  if (days > 0) {
+    difference += `${days} ${days > 1 ? "days" : "day"} `;
+  } else {
+    difference += `0 day `;
+  }
+
+  difference += `${hours} ${hours > 1 ? "hours" : "hour"} ${minutes} ${
+    minutes > 1 ? "minutes" : "minute"
+  }`;
+
+  return difference;
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -30,11 +63,13 @@ export default async function handler(
         // const dbNow = (): Date => dayjs().add(8, "hour").toDate();
         // const today = new Date(dbNow().setUTCHours(0, 0, 0, 0));
         // const tomorrow = new Date(dbNow().setUTCHours(24, 0, 0, 0));
-        const start = new Date(patient?.start);
-        const today = new Date();
+        // const start = new Date(patient?.start);
+        // const today = new Date();
 
-        const difference = today.getTime() - start.getTime();
-        const day = Math.ceil(difference / (1000 * 3600 * 24)) + 1;
+        // const difference = today.getTime() - start.getTime();
+        // const day = Math.ceil(difference / (1000 * 3600 * 24)) + 1;
+
+        const day = time_diff(new Date(patient?.start));
         const symptom = await prisma.symptom.findMany({
           where: {
             user: {
