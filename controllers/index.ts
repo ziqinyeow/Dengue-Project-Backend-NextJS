6b2e1cb96743ec21abc4fa5ac2272b43d933811f
Yeow Zi Qin <ziqinyeow@gmail.com>
2022-04-24 21:06:58 +0800

    update db controller

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,8 +1,6 @@
 import * as Detail from "./Detail.controller";
-import * as News from "./News.controller";
 import * as Quiz from "./Quiz.controller";
 import * as Symptom from "./Symptom.controller";
-import * as Task from "./Task.controller";
 import * as User from "./User.controller";
 
-export { Detail, News, Quiz, Symptom, Task, User };
+export { Detail, Quiz, Symptom, User };
